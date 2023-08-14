e5d86b8a4c6bba3fe86d187e333bcfdff6c8557e
Sharath Mahadev Bhat <sharubhat@gmail.com>
2023-08-14 13:24:19 -0700

    Solved 5th exercise in functions (#2)
    Co-authored-by: SHARATH BHAT <shabhat@adobe.com>

--- fpinkotlin-parent/fpinkotlin-functions-exercises/src/main/kotlin/com/fpinkotlin/functions/exercise05/Functions.kt
+++ fpinkotlin-parent/fpinkotlin-functions-exercises/src/main/kotlin/com/fpinkotlin/functions/exercise05/Functions.kt
@@ -11,7 +11,14 @@ fun <T, U, V> compose(f: (U) -> V, g: (T) -> U): (T) -> V = { f(g(it)) }
 
 val add: (Int) -> (Int) -> Int = { a -> { b -> a + b} }
 
-val compose = { f: (Int) -> Int -> { g: (Int) -> Int -> { n: Int -> f(g(n)) } } }
+val compose = { x: (Int) -> Int -> { y: (Int) -> Int -> { z: Int -> x(y(z)) } } }
 
-fun <T, U, V> higherCompose() = { f: (U) -> V -> { g: (T) -> U -> { x: T -> f(g(x)) } } }
-
+/* T is type of input x. U is the result type of g(x). V is the result type of f(g(x)).
+   higher compose is of the form f ° g ° x which means first apply g to input x, and then apply f to result of g
+   g: (T) -> U
+   f: (U) -> V
+   higherCompose: (type of f) -> (type of g) -> (type of input -> type of result)
+   higherCompose: ((U) -> V) -> ((T) -> U) -> ((T) -> V)
+ */
+fun <T, U, V> higherCompose(): ((U) -> V) -> ((T) -> U) -> (T) -> V = { f -> {g -> { f(g(it)) } } }
+fun <T, U, V> higherCompose2(): ((U) -> V) -> ((T) -> U) -> (T) -> V = { f -> {g -> { x -> f(g(x)) } } }  // more explicit
