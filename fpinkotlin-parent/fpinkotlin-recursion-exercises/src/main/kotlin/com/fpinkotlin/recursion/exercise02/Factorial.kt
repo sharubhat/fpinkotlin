16a1453c245099245bc2b18d504a886d2a9b85be
Sharath Mahadev Bhat <sharubhat@gmail.com>
2023-08-20 16:02:14 -0700

    Solved recursion problem 2 (#6)
    Co-authored-by: SHARATH BHAT <shabhat@adobe.com>

--- fpinkotlin-parent/fpinkotlin-recursion-exercises/src/main/kotlin/com/fpinkotlin/recursion/exercise02/Factorial.kt
+++ fpinkotlin-parent/fpinkotlin-recursion-exercises/src/main/kotlin/com/fpinkotlin/recursion/exercise02/Factorial.kt
@@ -1,5 +1,21 @@
 package com.fpinkotlin.recursion.exercise02
 
 object Factorial {
-    val factorial: (Int)-> Int = TODO("factorial")
+    /* option 1
+    lateinit var factorial: (Int) -> Int
+
+    init {
+        factorial = { if (it <= 1) it else it * factorial(it - 1) }
+    }
+    */
+
+    /* Option 2
+    val factorial: (Int) -> Int by lazy { { n: Int -> if (n <=1) n else n * factorial(n - 1) } }
+    */
+
+    private lateinit var fact: (Int) -> Int
+    init {
+        fact = { if (it <= 1) it else it * fact(it - 1) }
+    }
+    val factorial = fact
 }
