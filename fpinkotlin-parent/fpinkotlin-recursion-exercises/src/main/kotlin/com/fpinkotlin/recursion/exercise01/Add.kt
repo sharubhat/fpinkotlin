8d3df0cef0999bf469c85baea037ef862b85ca03
SHARATH BHAT <shabhat@adobe.com>
2023-08-20 22:14:03 -0700

    Fixed recursion exercise 1

--- fpinkotlin-parent/fpinkotlin-recursion-exercises/src/main/kotlin/com/fpinkotlin/recursion/exercise01/Add.kt
+++ fpinkotlin-parent/fpinkotlin-recursion-exercises/src/main/kotlin/com/fpinkotlin/recursion/exercise01/Add.kt
@@ -11,7 +11,7 @@ public static final int add(int a, int b) {
       return a;
    }
  */
-tailrec fun add(a: Int, b: Int): Int = if (b == 0) a else add(inc(a), dec(b))
+tailrec fun addWrong(a: Int, b: Int): Int = if (b == 0) a else addWrong(inc(a), dec(b))
 
 tailrec fun add2(a: Int, b: Int): Int =
     if (b == 0) a
@@ -21,3 +21,27 @@ tailrec fun add2(a: Int, b: Int): Int =
 fun inc(n: Int) = n + 1
 fun dec(n: Int) = n - 1
 
+tailrec fun add(a: Int, b: Int): Int =
+    when {
+        (b == 0) -> a
+        (b > 0) -> add(inc(a), dec(b))
+        else -> add(dec(a), inc(b))
+    }
+
+tailrec fun add(a: Long, b: Int): Long =
+    when {
+        (b == 0) -> a
+        (b > 0) -> add(incFixed(a), decFixed(b))
+        else -> add(decFixed(a), incFixed(b))
+    }
+
+fun incFixed(n: Long) = n + 1
+fun decFixed(n: Long): Long = n - 1
+
+fun incFixed(n: Int) = n + 1
+fun decFixed(n: Int) = n - 1
+
+fun main() {
+    println(add(3, -4))
+    println(add(4, -3))
+}
